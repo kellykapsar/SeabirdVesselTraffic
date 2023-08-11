4a2c1b2310dbf4babe8ae0fef83e156d0f710a7f
kellykapsar <kelly.kapsar@gmail.com>
2023-08-11 14:22:52 -0500

    Script to create summary tables for analytical results.

---
+++ R/3_results_tables.R
@@ -0,0 +1,35 @@
+region_summary_table <- function(fullDf, nestDf, region_title){
+  subDf <- fullDf[fullDf$hex_id %in% nestDf$fall[[1]]$hex_id,]
+  tb <- subDf %>% group_by(season) %>% summarize(n_hexes = length(unique(subDf$hex_id)),
+                                                 sample_area = sum(sample_area),
+                                                 n_transects = sum(n_transects),
+                                                 hrs_al = sum(hrs_al),
+                                                 n_hrs_al = sum(n_hrs_al),
+                                                 d_hrs_al = sum(d_hrs_al))
+  tb <- tb %>% mutate(region = region_title) %>% relocate(region)
+  return(tb)
+  # library(expss)
+  # tb <- tb %>% apply_labels(sample_area = "Area surveyed (km**2)",
+  #                           n_transects = "# of transects",
+  #                           hrs_al = "Total vessel traffic (hours)", 
+  #                           n_hrs_al = "Nighttime vessel traffic (hours)")
+  # tb %>% tab_pivot()
+}
+
+risk_summary_table <- function(nestDf, region_title){
+  t <- nestDf %>% 
+    mutate(region = region_title,
+           high_risk_summ = map_dbl(summer, ~sum(.$risk_cat %in% c("high", "veryhigh"))),
+           high_risk_fall = map_dbl(fall, ~sum(.$risk_cat %in% c("high", "veryhigh"))),
+           mean_risk_summ = round(map_dbl(summer, ~mean(.$risk_cont)),2),
+           mean_risk_fall = round(map_dbl(fall, ~mean(.$risk_cont)),2),
+           median_risk_summ = round(map_dbl(summer, ~median(.$risk_cont)),2),
+           median_risk_fall = round(map_dbl(fall, ~median(.$risk_cont)),2),
+           sd_risk_summ = round(map_dbl(summer, ~sd(.$risk_cont)),2),
+           sd_risk_fall = round(map_dbl(fall, ~sd(.$risk_cont)),2),
+           max_risk_summ = round(map_dbl(summer, ~max(.$risk_cont)),2),
+           max_risk_fall = round(map_dbl(fall, ~max(.$risk_cont)),2)
+          ) %>% 
+    select(-fall, -summer)
+  return(t)
+}
